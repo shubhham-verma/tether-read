e187c3e493e3ed77c5113bb67f353806c4374090
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-08-04 16:05:40 +0530

    Foxed navbar behaviour on small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 'use client';
 import { useAuth } from "@/context/AuthContext";
 import { useRouter, usePathname } from "next/navigation";
+import { useState } from "react";
 
 import toast from 'react-hot-toast';
 
@@ -8,6 +9,7 @@ export default function Navigation() {
     const { user, loading, logout } = useAuth();
     const router = useRouter();
     const pathname = usePathname();
+    const [menuOpen, setMenuOpen] = useState(false);
 
 
     const handleLogout = async () => {
@@ -50,13 +52,14 @@ export default function Navigation() {
                         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tether Read</span>
                         </a>
-                        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={menuOpen}
+                            onClick={() => setMenuOpen((prev) => !prev)}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
-                        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <div className={`${menuOpen ? "" : "hidden"} w-full md:block md:w-auto" id="navbar-default`}>
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
                                 {user ?
