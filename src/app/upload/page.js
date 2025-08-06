2fade52260b27310f82dee3e0916f204cef3b98f
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-08-06 18:10:59 +0530

    Added dropzone to upload page

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -37,9 +37,22 @@ function UploadPage() {
           <h1 className="text-3xl font-bold mb-6 ">Upload EPUB</h1>
 
           <form className="space-y-6  md:w-2/5">
-            <div>
+            {/* <div>
               <Label htmlFor="file" value="Select EPUB file" />
               <FileInput id="file" disabled />
+            </div> */}
+
+            <div class="flex items-center justify-center w-full">
+              <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-35 md:h-64 border-2 px-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                  </svg>
+                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF </p>
+                </div>
+                <input id="dropzone-file" type="file" class="hidden" />
+              </label>
             </div>
 
             <div className="border border-red-700 p-4 rounded-lg md:flex md:justify-evenly">
@@ -49,7 +62,7 @@ function UploadPage() {
               </div>
               <div className="md:flex md:flex-col md:justify-end">
                 <p className="mt-2"> <span className="font-medium">Title:</span> {dummyBook.title}</p>
-                  <p className="my-3 "> <span className="font-medium ">Author:</span> {dummyBook.author}</p>
+                <p className="my-3 "> <span className="font-medium ">Author:</span> {dummyBook.author}</p>
               </div>
             </div>
 
