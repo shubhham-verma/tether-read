49616d19d7e39ecbe9c9956027e60e265bb0641f
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-08-14 19:50:15 +0530

    added delete function

--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -1,6 +1,7 @@
 import { connectDB } from "@/lib/mongodb";
 import Book from "@/models/book";
 import { admin } from '@/lib/firebaseadmin';
+import mongoose from "mongoose";
 
 export default async function handler(req, res) {
 
@@ -16,33 +17,53 @@ export default async function handler(req, res) {
 
         await connectDB();
 
+        const { bookId, title, author, progress } = req.body;
+
         if (req.method === "GET") {
             const books = await Book.find({ userId: uid }).lean();
             return res.status(200).json({ total: books.length, books });
         }
 
-        else if (req.method === "POST") {
-            const { bookId, title, author, progress } = req.body;
-
+        else {
             if (!bookId)
                 return res.status(400).json({ error: "BookId must not be empty" });
+            else if (!mongoose.Types.ObjectId.isValid(bookId)) {
+                return res.status(400).json({ error: "Invalid book ID format" });
+            }
 
-            const book = await Book.findOneAndUpdate(
-                { _id: bookId, userId: uid },
-                {
-                    ...(title && { title }),
-                    ...(author && { author }),
-                    ...(progress && { progress }),
-                    updatedAt: new Date()
-                },
-                { new: true }
-            );
+            if (req.method === "PUT") {
+                const book = await Book.findOneAndUpdate(
+                    { _id: bookId, userId: uid },
+                    {
+                        ...(title && { title }),
+                        ...(author && { author }),
+                        ...(progress && { progress }),
+                        updatedAt: new Date()
+                    },
+                    { new: true }
+                );
 
-            if (!book)
-                return res.status(404).json({ error: "Book not found or not owned by user" });
+                if (!book)
+                    return res.status(404).json({ error: "Book not found or not owned by user" });
 
-            return res.status(200).json({ messgae: "Book updated successfully", book });
+                return res.status(200).json({ messgae: "Book updated successfully", book });
+            }
+
+            else if (req.method === "DELETE") {
+                const deletedBook = await Book.findOneAndDelete({ _id: bookId, userId: uid });
+
+                if (!deletedBook) {
+                    return res.status(404).json({ error: "Book not found or not owned by user" });
+                }
+
+                return res.status(200).json({ message: "Book deleted successfully" });
+            }
+            else
+                return res.status(405).json({ error: "Method not allowed" });
         }
+
+
+
     } catch (error) {
         if (error.code === "auth/id-token-expired") {
             return res.status(401).json({
@@ -54,6 +75,7 @@ export default async function handler(req, res) {
         }
         else {
             console.error("Error in GET /books:", error);
+            console.log(error.code);
             return res.status(500).json({ error: "Internal Server Error" });
         }
     }
