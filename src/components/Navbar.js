16af57c813a410a1b4bf7854c7a0a5629d8496cc
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-08-25 19:35:31 +0530

    Added hover to navbar on small screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,8 +49,8 @@ export default function Navigation() {
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
-                        <div className={`${menuOpen ? "" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
-                            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <div className={`${menuOpen ? "absolute top-14 left-0 z-50" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
+                            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-b-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
                                 {user ?
                                     <>
