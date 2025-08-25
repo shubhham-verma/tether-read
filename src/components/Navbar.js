f6c716ce9a0b90486786a9d8601777e8402137c4
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-08-25 16:08:24 +0530

    Added main logo icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,6 +39,7 @@ export default function Navigation() {
                 <nav className="bg-white border-gray-200 dark:bg-gray-900">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                            <span> <img src="https://www.svgrepo.com/show/405748/green-book.svg" className="w-5 h-5" alt="logo" /> </span>
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tether Read</span>
                         </a>
                         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={menuOpen} onClick={() => setMenuOpen((prev) => !prev)}
